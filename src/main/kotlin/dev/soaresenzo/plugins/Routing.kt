6b472fb35f1289671f5d493c2c0d95af3eb9af97
Enzo <enzosoares@protonmail.com>
2022-12-08 22:54:35 -0300

    Adding order routes

--- src/main/kotlin/dev/soaresenzo/plugins/Routing.kt
+++ src/main/kotlin/dev/soaresenzo/plugins/Routing.kt
@@ -1,6 +1,9 @@
 package dev.soaresenzo.plugins
 
 import dev.soaresenzo.routes.customerRouting
+import dev.soaresenzo.routes.getOrderRoute
+import dev.soaresenzo.routes.listOrdersRoute
+import dev.soaresenzo.routes.totalizeOrderRoute
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -10,5 +13,8 @@ import io.ktor.server.request.*
 fun Application.configureRouting() {
     routing {
         customerRouting()
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
     }
 }
